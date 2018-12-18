eaf86513a5b78d5821621c5cd1ddc319c8489a53
山泉农夫 <1942480291@qq.com>
2018-12-18 07:38:18 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,18 @@ class UsersController extends Controller
     	return view('users.create');
     }
 
+    //显示用户信息
     public function show(User $user){
     	return view('users.show',compact('user'));
+    }
+
+    //对注册表单进行处理
+    public function store(Request $request){
+    	$this->validate($request,[
+    		'name'=>'required|max:50',
+    		'email'=>'required|email|unique:users|max:255',
+    		'password'=>'required|confirmed|min:6'
+    	]);
+    	return;
     }
 }
