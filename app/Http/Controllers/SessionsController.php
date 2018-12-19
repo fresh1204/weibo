487f25a2ef4a977720480ab1bd73dd4463e6e4c2
山泉农夫 <1942480291@qq.com>
2018-12-19 04:11:07 +0000

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,7 +19,7 @@ class SessionsController extends Controller
     	]);
     	//var_dump(Auth::user());exit;
     	//数据库验证
-    	if(Auth::attempt($credentials)){
+    	if(Auth::attempt($credentials,$request->has('remember'))){
     		session()->flash('success','欢迎回来！');
     		return redirect()->route('users.show',[Auth::user()]);
     	}else{
