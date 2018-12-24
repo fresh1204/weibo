e0d1b5f0712e99822c0372a67432477aab04f095
山泉农夫 <1942480291@qq.com>
2018-12-24 03:09:50 +0000

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -34,9 +34,13 @@ class UsersController extends Controller
     	return view('users.create');
     }
 
-    //显示用户信息
+    //显示用户信息(加微博信息)
     public function show(User $user){
-    	return view('users.show',compact('user'));
+    	 //获取用户微博分页列表
+        $statuses = $user->statuses()->orderBy('created_at','desc')->paginate(10);
+
+    	//return view('users.show',compact('user'));
+    	return view('users.show',compact('user','statuses'));
     }
 
     //对注册表单进行处理
